9384d624967e7b102d75a97ea014de9d1ca15bbc
SirMengo <msimoes0103@gmail.com>
2025-05-21 14:20:36 +0100

    Added assets and some animations to the background

---
+++ bird.gd
@@ -0,0 +1,8 @@
+extends CharacterBody2D
+
+const GRAVITY :		int =	1000
+const MAX_VEL :		int =	600
+const FLAP_SPEED :	int =	-500
+var flying :		bool =	false
+var falling :		bool =	false
+const START_POS = 			Vector2(100, 400)
